a8e3f77533cb2ccf671a3995d69d3c53be919f84
macaljan_LinPC <macaljan@fel.cvut.cz>
2024-04-14 15:33:45 +0200

    hw05 done

--- hw05.py
+++ hw05.py
@@ -8,18 +8,20 @@ import matplotlib.image as mpimg
 import scipy.io as sio            # for matlab file format output
 import itertools                  # for generating all combinations
 
+TF_COLORS = True
 
 # These coords are in [x, y] format, FLIP IT WHEN INDEXING!!
 
+# points in img1
 U0 = np.array([[ 142.4,    93.4,   139.1,   646.9,  1651.4,  1755.2,  1747.3,  1739.5,  1329.2,   972.0],
                [1589.3,   866.7,   259.3,   305.6,    87.3,   624.8,  1093.5,  1593.8,  1610.2,  1579.3]])
-
+# points in img2
 U = np.array([[783.8,   462.6,   243.7,   363.9,   465.2,   638.3,   784.7,   954.6,   927.8,   881.4],
               [747.5,   671.2,   586.8,   463.9,   248.5,   260.6,   291.5,   326.9,   412.8,   495.0]])
-
+# missing patch bounds in img2
 C = np.array([[474.4,   508.2,   739.3,   737.2],
               [501.7,   347.0,   348.7,   506.7]])
-
+# pokemon sheet bounds in img2
 C2 = np.array([[473, 131, 798, 986],
                [226, 597, 807, 325]])
 
@@ -46,8 +48,8 @@ def p2e(p):
 def u2H(u, u0):
     A = np.zeros((8, 9))
     for i in range(4):
-        ui, vi = u[:, i]
-        u_i, v_i = u0[:, i]
+        ui, vi = u[:, i]      # img2 coords
+        u_i, v_i = u0[:, i]   # img1 coords
         A[2*i, :]   = np.array([ui, vi, 1,  0,  0, 0, -u_i*ui, -u_i*vi, -u_i])
         A[2*i+1, :] = np.array([ 0,  0, 0, ui, vi, 1, -v_i*ui, -v_i*vi, -v_i])
 
@@ -143,121 +145,134 @@ def match_hists(img, img_target, Cs=None):
 # ---------- using color transformation matrix ------------------------------
 
 def get_color_tf(img1, img2, H, limits):
-    """ Function, finds the color transformation matrix T
+    x_min, x_max, y_min, y_max = limits
 
-    Parameters
-    ----------
-    image1 : np.array (H, W, 3)
-        First image in RGB format
-    image2 : np.array (H, W, 3)
-        Second image in RGB format
-    H : np.array (3, 3)
-        Homography matrix
-    x_min : int
-        Minimum x coordinate of the rectangle
-    x_max : int
-        Maximum x coordinate of the rectangle
-    y_min : int
-        Minimum y coordinate of the rectangle
-    y_max : int
-        Maximum y coordinate of the rectangle
-
-    Returns
-    -------
-    np.array (4, 3)
-        Color transformation matrix
-    """
-
-    [x_min, x_max, y_min, y_max] = limits
-    xs = np.arange(x_min, x_max)
-    ys = np.arange(y_min, y_max)
-
-    patch_ref = np.zeros((len(ys), len(xs), 3), dtype=np.uint8)
+    img2_pixels = []
+    img1_pixels = []
 
     H_inv = np.linalg.inv(H)
-    for j, y in enumerate(ys):
-        for i, x in enumerate(xs):
-            x_new, y_new = p2e(H_inv @ [x, y, 1]).round().astype(int)
-            patch_ref[j, i] = img1[y_new, x_new, :]
+    for i in range(img2.shape[0]):
+        for j in range(img2.shape[1]):
+            # small_rect = in_rect([j, i], Cs[0])
+            # big_rect = in_rect([j, i], Cs[1])
+            # if not small_rect and big_rect:
+            if x_min <= j < x_max and y_min <= i < y_max:
+                px2 = np.array([j, i]).reshape(2, 1)            # u,x == j, v,y == i
+                px1 = np.round(p2e(H @ e2p(px2))).astype(int)
+                if 0 <= px1[1] < img1.shape[0] and 0 <= px1[0] < img1.shape[1]:
+                    img1_pixels.append(img1[px1[1, 0], px1[0, 0], :])
+                    img2_pixels.append(img2[px2[1, 0], px2[0, 0], :])
 
-    patch_target = img1[y_min:y_max, x_min:x_max, :]
+    img1_pixels = np.array(img1_pixels)
+    img2_pixels = np.array(img2_pixels)
 
-    r = patch_ref[:, :, 0].flatten()
-    g = patch_ref[:, :, 1].flatten()
-    b = patch_ref[:, :, 2].flatten()
+    r1, g1, b1 = np.hsplit(img1_pixels, 3)
+    r2, g2, b2 = np.hsplit(img2_pixels, 3)
 
-    r_target = patch_target[:, :, 0].flatten()
-    g_target = patch_target[:, :, 1].flatten()
-    b_target = patch_target[:, :, 2].flatten()
-
-    ones = np.ones_like(r)
-
-    B = np.c_[r, g, b, ones]
-    A = np.c_[r_target, g_target, b_target]
+    A = np.c_[r1, g1, b1, np.ones(r1.shape)]
+    B = np.c_[r2, g2, b2]
     T = np.linalg.lstsq(A, B, rcond=None)[0]
 
     return T
-
 
 if __name__ == "__main__":
     img1 = mpimg.imread("pokemon_00.jpg")
     img2 = mpimg.imread("pokemon_10.jpg")
 
     H, points_sel = u2h_optim(U, U0)
-    points_sel = points_sel + 1  # to matlab indexing
+    print(points_sel)
+    # points_sel = points_sel + 1  # to matlab indexing
     sio.savemat('05_homography.mat', {'u': U, 'u0': U0, "point_sel": points_sel, "H": H})
 
-    # limits = x_min, x_max, y_min, y_max
-    limits = [550, 800, 550, 700]
-    C_limits = np.array([[550, 550, 800, 800],
-                         [550, 700, 700, 550]])
-    T = get_color_tf(img1, img2, H, limits)
-    print(T)
+    if TF_COLORS:
+        # __color tf. matrix method__ <-- [better results]
+        # limits = x_min, x_max, y_min, y_max
+        limits = [550, 800, 550, 700]
+        T = get_color_tf(img1, img2, H, limits)
+        print(T)
+
+        # Cs = [C, C2]
+        # T = get_color_tf(img1, img2, H, Cs)
+
+        # __matching histograms method__ <-- [bad results]
+        # try:
+        #     img3 = np.load("img3.npy")
+        # except:
+        #     print("img3.npy doesn't exist --> creating img3")
+        #     img3 = match_hists(img1.copy(), img2, Cs)
+        #     np.save("img3.npy", img3)
 
     try:
-        img3 = np.load("img3.npy")
+        if TF_COLORS:
+            img2 = mpimg.imread("05_correctedn.png")
+        else:
+            img2 = mpimg.imread("05_corrected.png")
+        print("Loaded")
     except:
-        print("img3.npy doesn't exist --> creating img3")
-        Cs = [C, C2]
-        img3 = match_hists(img1.copy(), img2, Cs)
-        np.save("img3.npy", img3)
+        if TF_COLORS:
+            print("05_correctedn.png doesn't exist --> creating")
+        else:
+            print("05_corrected.png doesn't exist --> creating")
+        H_inv = np.linalg.inv(H)
+        for i in range(img2.shape[0]):
+            for j in range(img2.shape[1]):
+                if in_rect([j, i], C):
+                    px2 = np.array([j, i]).reshape(2, 1)  # u,x == j, v,y == i
+                    px1 = np.round(p2e(H @ e2p(px2))).astype(int)
+                    if 0 <= px1[1] < img1.shape[0] and 0 <= px1[0] < img1.shape[1]:
+                        r, g, b = img1[px1[1, 0], px1[0, 0], :]
+                        if TF_COLORS:
+                            img2[i, j, :] = (np.array([r, g, b, 1]) @ T).flatten()
+                        else:
+                            img2[i, j, :] = np.array([r, g, b])
 
-    plt.imshow(img1)
-    plt.show()
-    plt.imshow(img2)
-    plt.show()
-    plt.imshow(img3)
+        if TF_COLORS:
+            mpimg.imsave("05_correctedn.png", img2)
+        else:
+            mpimg.imsave("05_corrected.png", img2)
+
+    points_notsel = np.where(~np.isin(np.arange(10), points_sel))
+    fig, ax = plt.subplots(1, 2, figsize=(15, 8))
+    ax[0].imshow(img2)
+    ax[1].imshow(img1)
+    labeled = [False, False]
+    for i in range(U.shape[1]):
+        if i in points_sel:
+            b_box = dict(boxstyle="square", fc="g")
+            if labeled[0]:
+                ax[0].plot(U[0, i], U[1, i], "go", mec="k", mew=0.5)
+            else:
+                labeled[0] = True
+                ax[0].plot(U[0, i], U[1, i], "go", mec="k", mew=0.5, label="used for H")
+            ax[0].annotate(i+1, U[:, i], xytext=(0, 12), textcoords="offset points", color="k", fontweight="bold",
+                           bbox=b_box)
+            ax[1].plot(U0[0, i], U0[1, i], "go", mec="k", mew=0.5)
+            ax[1].annotate(i+1, U0[:, i], xytext=(0, 12), textcoords="offset points", color="k", fontweight="bold",
+                           bbox=b_box)
+        else:
+            b_box = dict(boxstyle="square", fc="r")
+            if labeled[1]:
+                ax[0].plot(U[0, i], U[1, i], "ro", mec="k", mew=0.5)
+            else:
+                labeled[1] = True
+                ax[0].plot(U[0, i], U[1, i], "ro", mec="k", mew=0.5, label="other points")
+
+            ax[0].annotate(i+1, U[:, i], xytext=(0, 12), textcoords="offset points", color="k", fontweight="bold",
+                           bbox=b_box)
+            ax[1].plot(U0[0, i], U0[1, i], "ro", mec="k", mew=0.5)
+            ax[1].annotate(i+1, U0[:, i], xytext=(0, 12), textcoords="offset points", color="k", fontweight="bold",
+                           bbox=b_box)
+
+    ax[0].set_title("Labelled points in my image")
+    ax[0].set_xlabel("x [px]")
+    ax[0].set_ylabel("y [px]")
+    ax[0].legend(loc="lower left")
+
+    ax[1].set_title("Labelled points in reference image")
+    ax[1].set_xlabel("x [px]")
+    ax[1].set_ylabel("y [px]")
+
+    plt.savefig('05_homography.pdf')
     plt.show()
 
-    H_inv = np.linalg.inv(H)
-    for i in range(img2.shape[0]):
-        for j in range(img2.shape[1]):
-            if in_rect([j, i], C):
-                px2 = np.array([j, i]).reshape(2, 1)            # u,x == j, v,y == i
-                px1 = np.round(p2e(H @ e2p(px2))).astype(int)
-                if 0 <= px1[1] < img1.shape[0] and 0 <= px1[0] < img1.shape[1]:
-                    # img2[i, j, :] = img1[px1[1], px1[0], :]
-                    color = img3[px1[1], px1[0], :].reshape(3, 1)
-                    img2[i, j, :] = (T @ e2p(color)).flatten()
-
-    plt.figure()
-    plt.imshow(img2)
-    U0_proj = p2e(H_inv @ e2p(U0))
-    plt.plot(C[0], C[1], "rx")
-    plt.plot(C_limits[0], C_limits[1], "gx")
-    plt.plot(U[0], U[1], "go")
-    plt.plot(U0_proj[0], U0_proj[1], "mo")
-    plt.show()
-
-    plt.imshow(img1)
-    U_proj = p2e(H @ e2p(U))
-    C_proj = np.round(p2e(H @ e2p(C))).astype(int)
-    C_limits_proj = np.round(p2e(H @ e2p(C_limits))).astype(int)
-    plt.plot(C_proj[0], C_proj[1], "rx")
-    plt.plot(C_limits_proj[0], C_limits_proj[1], "gx")
-    plt.plot(U0[0], U0[1], "go")
-    plt.plot(U_proj[0], U_proj[1], "mo")
-    plt.show()
-
-    mpimg.imsave("05_corrected.png", img2)
 
